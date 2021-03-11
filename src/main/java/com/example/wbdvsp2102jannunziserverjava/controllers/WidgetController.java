a303993d49aabb423d3572ce3934509c3d9c6af8
JoseAnnunziatoSonos <jose.annunziato@sonos.com>
2021-03-10 21:56:54 -0500

    widgets

---
+++ src/main/java/com/example/wbdvsp2102jannunziserverjava/controllers/WidgetController.java
@@ -0,0 +1,36 @@
+package com.example.wbdvsp2102jannunziserverjava.controllers;
+
+import com.example.wbdvsp2102jannunziserverjava.models.Widget;
+import com.example.wbdvsp2102jannunziserverjava.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+    @Autowired
+    WidgetService service;
+    
+    @PostMapping("/api/topics/{tid}/widgets")
+    public Widget createWidgetForTopic(
+            @PathVariable("tid") String topicId,
+            @RequestBody Widget widget
+    ) {
+        return service.createWidgetForTopic(topicId, widget);
+    }
+    
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets() {
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/api/topics/{tid}/widgets")
+    public List<Widget> findWidgetsForTopic(
+            @PathVariable("tid") String topicId
+    ) {
+        return service.findWidgetsForTopic(topicId);
+    }
+}
